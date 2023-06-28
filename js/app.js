ac7a9b5d1f20d255f2bff4ebb5acf0036a212940
Dhaifullah HIlmy <dhaifullahhilmy@gmail.com>
2023-06-28 17:34:57 +0700

    Ten Commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling:true,
+  anchors:['page'],
+  controlArrows: false
+});
+
+gsap.from("#nama", {duration: 5, text: ""});
+gsap.to("#nama", {duration: 5, text: "Developer", delay: 5});
