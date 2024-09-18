fa577568353ae8f259e5f034b40d9ad0ea36ec86
Dhaifullah HIlmy <dhaifullahhilmy@gmail.com>
2024-09-18 16:39:56 +0700

    Twelveth 4th

--- src/components/Slide2.jsx
+++ src/components/Slide2.jsx
@@ -1,7 +1,7 @@
 import Skill from './Skill';
 
 const Slide2 = () => (
-  <div className="container text-light mb-5 pb-5">
+  <div className="container text-light mt-3 mb-5 pb-5">
     <p className="display-4 fw-bolder text-lg-start text-center">About Me</p>
     <p className="fs-5">Saya adalah fresh graduate yang berkomitmen untuk meniti karir sebagai Software Developer dengan minat khusus pada pengembangan web, khususnya front-end. Menguasai HTML, CSS, dan JavaScript, saya memiliki pengalaman menciptakan antarmuka responsif dan berorientasi pada pengguna. Saya memiliki kemampuan komunikasi yang baik, mudah beradaptasi, serta senang bekerja dalam tim. Saya siap berkontribusi dengan ide-ide kreatif dan solusi inovatif di dunia digital.</p>
     <h4 className="mt-2 fw-bold text-lg-start text-center">My Skill</h4>
